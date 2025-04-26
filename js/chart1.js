9142abeeea2d483b5c2712ef3381dbaad3c9c44f
mohamed salad <abdikariimsalad55@gmail.com>
2025-04-26 13:11:34 +0300

    adding service chart display

--- js/chart1.js
+++ js/chart1.js
@@ -1,29 +1,85 @@
-const service = {
-  jan: 2,
-  feb: 100,
-  mar: 20,
-  apr: 30,
+// const service = {
+//   jan: 2,
+//   feb: 100,
+//   mar: 20,
+//   apr: 30,
 
+// }
+
+
+
+
+
+// const ctx = document.getElementById('service-chart');
+
+// new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: Object.keys(service),
+//     datasets: [{
+//       label: ' services',
+//       data: Object.values(service),
+    
+//     }]
+//   },
+//   options: {
+//   responsive: true
+//   }
+// });
+
+
+
+saveCurrentMonthIncome()
+function saveCurrentMonthIncome() {
+  // Get the total income from localStorage
+  const serviceCustomers = JSON.parse(localStorage.getItem("customers")) || 0;
+  console.log("serviceCustomers, ", serviceCustomers.length)
+  // serviceCustomers.forEach(cust =>{
+  //   console.log("cus, ", cust)
+  // })
+
+  // Retrieve or initialize the monthly income data
+  const monthlyIncome = JSON.parse(localStorage.getItem("monthlyService")) || {};
+
+  // Get the current month
+  const currentDate = new Date();
+  const monthNames = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+  const currentMonth = monthNames[currentDate.getMonth()];
+
+  // Update the current month's income with the new totalIncome
+  monthlyIncome[currentMonth] = serviceCustomers.length;
+
+  // Save updated monthly income back to localStorage
+  localStorage.setItem("monthlyService", JSON.stringify(monthlyIncome));
 }
 
 
 
+const serviceChart = document.getElementById('service-chart');
 
+// Fetch saved monthly income
+const monthlyService = JSON.parse(localStorage.getItem("monthlyService")) || {};
 
-const ctx = document.getElementById('service-chart');
+// List of months
+const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
 
-new Chart(ctx, {
+// Prepare the balance for the chart by getting monthly income for each month
+const Services = Months.map(month => monthlyService[month] || 0);
+
+// Create the chart
+new Chart(serviceChart, {
   type: 'bar',
   data: {
-    labels: Object.keys(service),
+    labels: Months,
     datasets: [{
-      label: ' services',
-      data: Object.values(service),
-    
+      label: 'Monthly register services',
+      data: Services,
+      borderWidth: 1,
+      backgroundColor: '#587ef4'
     }]
   },
   options: {
-  responsive: true
+    responsive: true
   }
 });
 
