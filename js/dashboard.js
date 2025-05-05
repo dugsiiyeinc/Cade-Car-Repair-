e1ec91141430d2f88a6feb4d3704b75ee664026e
mohamed salad <abdikariimsalad55@gmail.com>
2025-05-05 20:27:39 +0300

    delete someSpace in dashboard js

--- js/dashboard.js
+++ js/dashboard.js
@@ -91,11 +91,7 @@ sidebarLinks.forEach((link) => {
 function addCustomerForm(event) {
   event.preventDefault();
 
-  //    date formating
-  //      const now =new Date();
-  // const formattedDate = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')} ${now.getHours().toString().padStart(2, '0')}:${now.getMinutes().toString().padStart(2, '0')}:${now.getSeconds().toString().padStart(2, '0')}`;
-  // console.log(formatted); // Example: "2025-03-28 14:45:30"
-
+ 
   const addCustomerData = {
     id: Date.now(),
     name: custFullName.value.trim(),
