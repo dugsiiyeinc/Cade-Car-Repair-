e9576660ffd0214345a296e05d0742eb501fdf3d
mohamed salad <abdikariimsalad55@gmail.com>
2025-06-03 11:17:09 +0300

    fix char.js service

--- js/chart1.js
+++ js/chart1.js
@@ -1,53 +1,4 @@
 
-// saveCurrentMonthService()
-// function saveCurrentMonthService () {
-//   const serviceCustomers = JSON.parse(localStorage.getItem("customers")) || [];
-//   const currentDate = new Date();
-//   const currentMonth = currentDate.getMonth(); // 0 - 11
-//   const currentYear = currentDate.getFullYear();
-
-//   const newThisMonth = serviceCustomers.filter(cust => {
-//     if (!cust.createdAt) return false;
-//     const createdAtDate = new Date(cust.createdAt);
-//     return createdAtDate.getMonth() === currentMonth && createdAtDate.getFullYear() === currentYear;
-//   });
-// //
-//   const monthNames = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-//   const monthKey = monthNames[currentMonth];
-
-//   const monthlyIncome = JSON.parse(localStorage.getItem("monthlyService")) || {};
-//   monthlyIncome[monthKey] = newThisMonth.length;
-
-//   localStorage.setItem("monthlyService", JSON.stringify(monthlyIncome));
-// }
-
-// const serviceChart = document.getElementById('service-chart');
-// const monthlyService = JSON.parse(localStorage.getItem("monthlyService")) || {};
-// const Months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-// const Services = Months.map(month => monthlyService[month] || 0);
-
-// new Chart(serviceChart, {
-//   type: 'bar',
-//   data: {
-//     labels: Months,
-//     datasets: [{
-//       label: 'Monthly register services',
-//       data: Services,
-//       borderWidth: 1,
-//       backgroundColor: '#587ef4'
-//     }]
-//   },
-//     options: {
-//     responsive: true,
-//     animation: false,
-//     transitions: {
-//       active: { animation: false },
-//       show: { animation: false },
-//       hide: { animation: false }
-//     }
-//   }
-// });
-
 let serviceChartInstance; // Global variable to hold the chart
 
 function saveCurrentMonthService() {
@@ -55,7 +6,7 @@ function saveCurrentMonthService() {
   const currentDate = new Date();
   const currentMonth = currentDate.getMonth();
   const currentYear = currentDate.getFullYear();
-=======
+
 
 
   const newThisMonth = serviceCustomers.filter(cust => {
